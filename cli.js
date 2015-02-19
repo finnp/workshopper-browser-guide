421c53a6a2bb3f99087ab0c4bfe5f704ed5ef3fe
Finn <derfinn@gmail.com>
2015-02-19 17:07:12 +0100

    options help

--- cli.js
+++ cli.js
@@ -3,4 +3,16 @@
 var createGuide = require('./')
 var minimist = require('minimist')
 
-createGuide(process.cwd(), minimist(process.argv.splice(2)))
+var opts = minimist(process.argv.splice(2))
+
+if(opts.h || opts.help) {
+  console.log('usage: workshopper-browser-guide')
+  console.log('options:')
+  console.log('\t--output\n\t\toutput dir (defaults to ./guide)')
+  console.log('\t--name\n\t\tapp name (defaults to package.json name)')
+  console.log('\t--title\n\t\tapp title (defaults to app name)')
+  console.log('\t--color\n\t\tcolor style (defaults to blue)')
+  process.exit()
+}
+
+createGuide(process.cwd(), opts)
--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
 # workshopper-browser-guide
 [![NPM](https://nodei.co/npm/workshopper-browser-guide.png)](https://nodei.co/npm/workshopper-browser-guide/)
+
+## usage
+```
+$ workshopper-browser-guide --help
+usage: workshopper-browser-guide
+options:
+	--output
+		output dir (defaults to ./guide)
+	--name
+		app name (defaults to package.json name)
+	--title
+		app title (defaults to app name)
+	--color
+```
